a9bc3662ca5865d8663f854e824a98cb7e1721ad
Andreas Okholm <aokholm@gmail.com>
2017-02-22 11:07:52 +0100

    Initial Commit

---
+++ kiteXGroundControl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  kiteXGroundControl
+//
+//  Created by Andreas Okholm on 22/02/2017.
+//  Copyright © 2017 Andreas Okholm. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
